15b39db952eeb2fd31ad46ad587bc2878bcf9b16
jacksonllee <jacksonlunlee@gmail.com>
2019-11-27 10:29:22 -0600

    Better organize the score output files

--- 3_fairseq/train-sweep.sh
+++ 3_fairseq/train-sweep.sh
@@ -7,14 +7,18 @@ for dim in 8 16 32 64 128; do
 	done
 done
 
-for lang in $(ls data-bin); do
-	for dim in 8 16 32 64 128; do
-		./evaluate-one-dev.sh "$lang" "$dim" > "dev-scores-$lang-$dim.txt"
-	done
-done
+mkdir -p dev-scores
 
 for lang in $(ls data-bin); do
 	for dim in 8 16 32 64 128; do
-		./evaluate-one-test.sh "$lang" "$dim" > "test-scores-$lang-$dim.txt"
+		./evaluate-one-dev.sh "$lang" "$dim" > dev-scores/"dev-scores-$lang-$dim.txt"
+	done
+done
+
+mkdir -p test-scores
+
+for lang in $(ls data-bin); do
+	for dim in 8 16 32 64 128; do
+		./evaluate-one-test.sh "$lang" "$dim" > test-scores/"test-scores-$lang-$dim.txt"
 	done
 done
